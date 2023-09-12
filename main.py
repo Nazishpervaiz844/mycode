ba012453c054577143f61cdddfb9196a10d43d81
Nazishpervaiz844 <nazishpervaiz844@gmail.com>
2023-09-12 18:17:40 +0500

    hi

---
+++ main.py
@@ -0,0 +1,34 @@
+# find area of rectangle where length is 5 and width is 8
+
+length = 5
+width = 8
+area = length * width
+print (" the area of rectangle is: ", area)
+
+# find area of traingle where the sides are of 8,9,10 cm
+b = 8
+h = 9
+w= 10
+Area= 0.5 * b * h * w
+print (" the area of triangle is: ", Area)
+
+# find area of circle where the radius is 3
+
+pi = 3.14 
+r= 3
+areaOfcircle = pi * ( r * r)
+print (" the area of circle is: ", areaOfcircle)
+
+# convert temperatures from celcuis to fahrenheit 
+
+c =float (input("enter temp in celcius: "))
+f = (0.5 * c) + 32
+
+print ("temp in fahrenheit:", f)
+
+#  from fahrehnheit to celcius
+f = 45
+c = (f - 32) * 1.8
+print("temp in celcius: ", c)
+
+
